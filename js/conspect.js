d51e90ec6e181ab731074700f8b87cd37edde09e
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-06-24 12:47:00 +0300

    Примеры из конспекта

---
+++ js/conspect.js
@@ -0,0 +1,54 @@
+/*let clientCounter = 10;
+const maxClients = 25;
+
+while (clientCounter < maxClients) {
+  console.log(clientCounter);
+  clientCounter += 1;
+}*/
+
+/*let password = '';
+
+do {
+  password = prompt('Введите пароль длиннее 4-х символов', '');
+} while (password.length < 5);
+
+console.log('Ввели пароль: ', password);*/
+
+/*for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
+}*/
+
+/*const target = 4;
+let sum = 0;
+
+for (let i = 0; i <= target; i += 1) {
+  console.log(i);
+  sum += i;
+}
+
+console.log(sum);*/
+
+/*const max = 10;
+for (let i = 0; i < max; i += 1) {
+  console.log(`${max} % ${i} = `, max % i);
+}*/
+
+/*const max = 10;
+
+for (let i = 0; i < max; i += 1) {
+  if (i % 2 === 0) {
+    continue;
+  }
+  console.log('Нечетное i: ', i);
+}*/
+
+const max = 10;
+let sum;
+for (let i = 1; i <= max; i += 1) {
+  if (i % 2 !== 0) {
+    continue;
+  }
+  console.log('Четное i: ', i);
+  let sum = i + i;
+  console.log('Cумма: ', sum);
+}
