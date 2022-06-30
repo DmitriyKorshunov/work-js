ea8744c666b0b11eef51ecd985790e999fa99b64
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-06-30 13:35:57 +0300

    Функция поиска самого длинного слова в строке

---
+++ js/longestword.js
@@ -0,0 +1,14 @@
+function findLongestWord(string) {
+  const stringArray = string.split(' ');
+  console.log(stringArray);
+  let longestWord = stringArray[0];
+  console.log(longestWord);
+  for (let i = 0; i < stringArray.length; i += 1) {
+    if (stringArray[i].length > longestWord.length) {
+      longestWord = stringArray[i];
+      console.log(longestWord);
+    }
+  }
+  return longestWord;
+}
+findLongestWord('The quick brown fox jumped over the lazy dog');
