e6e3485e88dcbd502a8281e6676c6df6b8d1e2cb
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-07-01 13:30:54 +0300

    В тренер добавлена задача на инклудс

--- js/trener.js
+++ js/trener.js
@@ -25,24 +25,45 @@
 // возвращает новый массив, в котором будут только те элементы массива numbers,
 // которые больше чем значение параметра value(число).
 
-function filterArray(numbers, value) {
-  console.log(numbers);
-  let namberOne = numbers[0];
-  //console.log('namberOne: ', namberOne);
-  let namberLast = numbers[numbers.length - 1];
-  //console.log('namberLast: ', namberLast);
-  let arrNumbers = [];
+// function filterArray(numbers, value) {
+//   console.log(numbers);
+//   let namberOne = numbers[0];
+//   //console.log('namberOne: ', namberOne);
+//   let namberLast = numbers[numbers.length - 1];
+//   //console.log('namberLast: ', namberLast);
+//   let arrNumbers = [];
 
-  for (const number of numbers) {
-    //console.log(i);
-    if (number[i] > value) {
-      arrNumbers.push(number[i]);
-      console.log(value);
-      console.log('arrNumbers: ', arrNumbers);
+//   for (const number of numbers) {
+//     //console.log(i);
+//     if (number[i] > value) {
+//       arrNumbers.push(number[i]);
+//       console.log(value);
+//       console.log('arrNumbers: ', arrNumbers);
+//     }
+//   }
+//   return narrNumbers;
+// }
+
+// filterArray([1, 2, 3, 4, 5], 3);
+// filterArray([12, 24, 8, 41, 76], 38);
+
+// Напиши функцию includes(array, value), которая делает тоже самое,
+// что и метод массива массив.includes(значение) - проверяет, есть ли в массиве
+// array значение value, возвращая true если есть и false в противном случае.
+// При выполнении этой задачи в теле функции includes() нельзя использовать
+// метод массив.includes(значение).
+
+function includes(array, value) {
+  for (let i = 0; i < array.length; i += 1) {
+    // console.log(i);
+    // console.log(array[i]);
+    if (array[i] === value) {
+      //console.log(array[i]);
+      //console.log(value);
+      return array[i];
     }
   }
-  return narrNumbers;
 }
-
-//filterArray([1, 2, 3, 4, 5], 3);
-filterArray([12, 24, 8, 41, 76], 38);
+console.log(includes);
+includes([1, 2, 3, 4, 5], 3);
+includes(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Jupiter');
