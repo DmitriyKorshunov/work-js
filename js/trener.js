92d02fff73f38385c6dd761fdd692169e7650c65
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-07-07 18:19:41 +0300

    Перебор массива однотип объектов

--- js/trener.js
+++ js/trener.js
@@ -126,19 +126,36 @@
 // }
 
 // ПЕРЕБОР массива однотипных объектов
-const colors = [
-  { hex: '#f44336', rgb: '244,67,54' },
-  { hex: '#2196f3', rgb: '33,150,243' },
-  { hex: '#4caf50', rgb: '76,175,80' },
-  { hex: '#ffeb3b', rgb: '255,235,59' },
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+// console.table(hexColors);
+// console.log(rgbColors);
+
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
 ];
 
-const hexColors = [];
-const rgbColors = [];
-
-for (const color of colors) {
-  hexColors.push(color.hex);
-  rgbColors.push(color.rgb);
+function getProductPrice(productName) {
+  // Change code below this line
+  let ollValProd = [];
+  for (const product of products) {
+    ollValProd.push(product.productName);
+    console.log(ollValProd);
 }
-console.table(hexColors);
-console.log(rgbColors);
+console.log(getProductPrice('price'));
+///getAllPropValues('name');
