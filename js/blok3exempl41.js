801feedda5abfdd158861a9089caaf69ea1bdc33
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-07-09 17:29:18 +0300

    Задача 41 блок 3 методы массива объектов, решено

--- js/blok3exempl41.js
+++ js/blok3exempl41.js
@@ -4,50 +4,51 @@ const atTheOldToad = {
     { name: 'Dragon breath', price: 780 },
     { name: 'Stone skin', price: 520 },
   ],
-  // Change code below this line
-  //   getPotions(...potions) {
-  //     return this.potions;
-  //   },
-  //   addPotion(newPotion) {
-  //     //console.log(newPotion);
 
-  //     for (const potion of this.potions) {
-  //       //   console.log(potion);
-  //       //   console.log(potion.name);
-  //       //   console.log(newPotion.name);
-  //       if (potion.name === newPotion.name) {
-  //         return `Error! Potion ${newPotion.name} is already in your inventory!`;
-  //       }
-  //     }
-  //     this.potions.push(newPotion);
-  //     //console.log(this.potions);
-  //   },
-  //   removePotion(potionName) {
-  //     console.log(potionName);
-  //     for (const potion of this.potions) {
-  //       const potionIndex = this.potions.indexOf(potion);
-  //       console.log(potionIndex);
-  //       if (potion.name === potionName) {
-  //         this.potions.splice(potionIndex, 1);
-  //       }
-  //     }
-  //     return `Potion ${potionName} is not in inventory!`;
-  //   },
-  updatePotionName(oldName, newName) {
-    console.log(oldName);
-    console.log(newName);
+  getPotions(...potions) {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    //console.log(newPotion);
 
+    for (const potion of this.potions) {
+      console.log(potion);
+      console.log(potion.name);
+      console.log(newPotion.name);
+      if (potion.name === newPotion.name) {
+        return `Error! Potion ${newPotion.name} is already in your inventory!`;
+      }
+    }
+    this.potions.push(newPotion);
+    console.log(this.potions);
+    return this.potions;
+  },
+  removePotion(potionName) {
+    console.log(potionName);
     for (const potion of this.potions) {
       const potionIndex = this.potions.indexOf(potion);
       console.log(potionIndex);
-      if (oldName === newName) {
-        return `Potion ${oldName} is not in inventory!`;
+      if (potion.name === potionName) {
+        this.potions.splice(potionIndex, 1);
+        return this.potions;
       }
-      this.potions.splice(potionIndex, 1, newName);
     }
+    return `Potion ${potionName} is not in inventory!`;
+  },
+  updatePotionName(oldName, newName) {
+    //console.log(oldName);
+    //console.log(newName);
+
+    for (const potion of this.potions) {
+      if (potion.name === oldName) {
+        potion.name = newName;
+        return this.potions;
+      }
+    }
+    return `Potion ${oldName} is not in inventory!`;
   },
 };
-console.log(atTheOldToad.updatePotionName('Dragon breath', 'Polymorth'));
+console.log(atTheOldToad.removePotion('Dragon breath'));
 //atTheOldToad.addPotion({ name: "Invisibility", price: 620 }),
 //в массиве potions последним элементом будет этот объект
 // atTheOldToad.addPotion({ name: "Dragon breath", price: 700 }),
